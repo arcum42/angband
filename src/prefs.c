4e27215e2e653163740466963cb9041650b00384
Shanoah Alkire <arcum42@gmail.com>
2010-12-24 20:28:58 -0800

    Revert a debugging message left in accidentally.

--- src/prefs.c
+++ src/prefs.c
@@ -1237,7 +1237,6 @@ bool process_pref_file(const char *name, bool quiet)
 		path_build(buf, sizeof(buf), ANGBAND_DIR_USER, name);
 
 	f = file_open(buf, MODE_READ, -1);
-	printf("Opening '%s'.\n", buf);
 	if (!f)
 	{
 		if (!quiet)
